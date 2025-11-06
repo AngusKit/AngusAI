592ee83bf64ac181528da9ba8ef4d180a7413b9a
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-11-06 10:23:58 +0800

    feat: add MySQL and PostgreSQL repository interfaces for ApiEndpointCallLog

---
+++ service/core/src/main/java/cloud/xcan/angus/core/ai/infra/persistence/postgres/apis/ApiEndpointCallLogRepoPostgres.java
@@ -0,0 +1,9 @@
+package cloud.xcan.angus.core.ai.infra.persistence.postgres.apis;
+
+import cloud.xcan.angus.core.ai.domain.apis.ApiEndpointCallLogRepo;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ApiEndpointCallLogRepoPostgres extends ApiEndpointCallLogRepo {
+
+}
