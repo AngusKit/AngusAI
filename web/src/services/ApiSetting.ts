de9e451d9b724d7afd16dce34232c24d2413337d
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-11-13 20:25:09 +0800

    feat: add ApiSettingTypes and ApiSetting for managing API schema and server configurations

---
+++ web/src/services/ApiSetting.ts
@@ -0,0 +1,57 @@
+import {AI, ApiLocaleResult} from '@xcan-angus/infra';
+import { OpenAPIV3_1 } from '@/types/openapi-types';
+import { ContentType, HttpClient, RequestParams } from './HttpClient.ts';
+import {ApiSchemaResult} from "@/services/ApiSettingTypes.ts";
+
+export class ApiSetting<SecurityDataType = unknown> {
+  http: HttpClient<SecurityDataType>;
+
+  constructor(http: HttpClient<SecurityDataType>) {
+    this.http = http;
+  }
+
+  /**
+   * @description 更新接口集服务器信息
+   *
+   * @tags 接口集设置
+   * @name ApiServersUpdate
+   * @summary 更新接口集服务器
+   * @request PUT:/api/v1/api-collections/{collectionId}/servers
+   * @secure
+   */
+  apiServersUpdate = (
+    collectionId: number,
+    data: OpenAPIV3_1.ServerObject[],
+    params: RequestParams = {},
+  ) =>
+    this.http.request<ApiSchemaResult, ApiLocaleResult>({
+      path: `${AI}/api/v1/api-collections/${collectionId}/servers`,
+      method: "PUT",
+      body: data,
+      secure: true,
+      type: ContentType.Json,
+      ...params,
+    });
+  /**
+   * @description 更新接口集安全方案信息
+   *
+   * @tags 接口集设置
+   * @name ApiSecuritiesUpdate
+   * @summary 更新接口集安全方案
+   * @request PUT:/api/v1/api-collections/{collectionId}/securities
+   * @secure
+   */
+  apiSecuritiesUpdate = (
+    collectionId: number,
+    data: Record<string, OpenAPIV3_1.SecuritySchemeObject>,
+    params: RequestParams = {},
+  ) =>
+    this.http.request<ApiSchemaResult, ApiLocaleResult>({
+      path: `${AI}/api/v1/api-collections/${collectionId}/securities`,
+      method: "PUT",
+      body: data,
+      secure: true,
+      type: ContentType.Json,
+      ...params,
+    });
+}
