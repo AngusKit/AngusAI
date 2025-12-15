aed5ce51bc038f6e2294ced5388aecfe9cdba911
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-12-15 20:52:53 +0800

    feat: update OpenAPI SDK generation script to reference AngusAI API documentation

--- web/scripts/generate-openapi-sdk.sh
+++ web/scripts/generate-openapi-sdk.sh
@@ -9,5 +9,5 @@ swagger-typescript-api generate  \
 --modular \
 --single-http-client \
 --module-name-index="2" \
---path="../docs/AngusGit-Apis-V1.json" \
+--path="../docs/AngusAI-Apis-V1.json" \
  --output="./temp/openapi-sdk"
