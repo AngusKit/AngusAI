384b6a8887d7d4eb02ab63428a8acf32f1fb5726
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2025-12-18 19:22:14 +0800

    feat: update OpenAPI SDK generation script to use first tag for module naming

--- web/scripts/generate-openapi-sdk.sh
+++ web/scripts/generate-openapi-sdk.sh
@@ -2,12 +2,22 @@
 ## Usage: cd AngusAI/web && sh ./scripts/generate-openapi-sdk.sh
 ## Note: Use v22.9.0+
 
+#swagger-typescript-api generate  \
+#--axios \
+#--extract-enums \
+#--http-client="axios" \
+#--modular \
+#--single-http-client \
+#--module-name-index="2" \
+#--path="../docs/AngusAI-Apis-V1.json" \
+# --output="./temp/openapi-sdk"
+
 swagger-typescript-api generate  \
 --axios \
 --extract-enums \
 --http-client="axios" \
 --modular \
 --single-http-client \
---module-name-index="2" \
+--module-name-first-tag \
 --path="../docs/AngusAI-Apis-V1.json" \
  --output="./temp/openapi-sdk"
