522e449e898a4f9e530f15746158b49a8e46a510
LiuXiaoLong <75962798+xcancloud@users.noreply.github.com>
2026-03-15 14:54:02 +0800

    feat: delete UsageStatistics

--- service/core/src/main/java/cloud/xcan/angus/core/ai/infra/persistence/mysql/analytics/UsageStatisticsRepoMysql.java
+++
@@ -1,9 +0,0 @@
-package cloud.xcan.angus.core.ai.infra.persistence.mysql.analytics;
-
-import cloud.xcan.angus.core.ai.domain.analytics.UsageStatisticsRepo;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsageStatisticsRepoMysql extends UsageStatisticsRepo {
-
-}
--- service/core/src/main/java/cloud/xcan/angus/core/ai/infra/persistence/postgres/analytics/UsageStatisticsRepoPostgres.java
+++
@@ -1,9 +0,0 @@
-package cloud.xcan.angus.core.ai.infra.persistence.postgres.analytics;
-
-import cloud.xcan.angus.core.ai.domain.analytics.UsageStatisticsRepo;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UsageStatisticsRepoPostgres extends UsageStatisticsRepo {
-
-}
